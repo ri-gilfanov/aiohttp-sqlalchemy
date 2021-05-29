68ed7b4675c8c0da6158009eaa56a1987e1a49f9
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-05-29 11:42:49 +0500

    Add SAViewMixin.sa_model attr

--- aiohttp_sqlalchemy/views/mixins.py
+++ aiohttp_sqlalchemy/views/mixins.py
@@ -1,15 +1,16 @@
-
 from typing import TYPE_CHECKING
 from aiohttp_sqlalchemy.constants import DEFAULT_KEY
 
 if TYPE_CHECKING:
     from aiohttp.web import Request
     from sqlalchemy.ext.asyncio import AsyncEngine
+    from typing import Any
 
 
 class SAViewMixin:
     """ SQLAlchemy class based view mixin. """
     request: 'Request'
+    sa_model: 'Any'  # Not all developers use declarative mapping
 
     @property
     def sa_main_session(self) -> 'AsyncEngine':
