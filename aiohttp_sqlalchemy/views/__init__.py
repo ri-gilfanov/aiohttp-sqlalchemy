631e608c96b6fada01c9e787a93f9a6cc6903287
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-05-29 10:39:25 +0500

    Move SAViewMixin from views/__init.py to views/mixins.py

--- aiohttp_sqlalchemy/views/__init__.py
+++ aiohttp_sqlalchemy/views/__init__.py
@@ -1,19 +1,5 @@
 from aiohttp.web import View
-from typing import TYPE_CHECKING
-from aiohttp_sqlalchemy.constants import DEFAULT_KEY
-
-if TYPE_CHECKING:
-    from aiohttp.web import Request
-    from sqlalchemy.ext.asyncio import AsyncEngine
-
-
-class SAViewMixin:
-    """ SQLAlchemy class based view mixin. """
-    request: 'Request'
-
-    @property
-    def sa_main_session(self) -> 'AsyncEngine':
-        return self.request[DEFAULT_KEY]
+from aiohttp_sqlalchemy.views.mixins import SAViewMixin
 
 
 class SAView(View, SAViewMixin):
---
+++ aiohttp_sqlalchemy/views/mixins.py
@@ -0,0 +1,16 @@
+
+from typing import TYPE_CHECKING
+from aiohttp_sqlalchemy.constants import DEFAULT_KEY
+
+if TYPE_CHECKING:
+    from aiohttp.web import Request
+    from sqlalchemy.ext.asyncio import AsyncEngine
+
+
+class SAViewMixin:
+    """ SQLAlchemy class based view mixin. """
+    request: 'Request'
+
+    @property
+    def sa_main_session(self) -> 'AsyncEngine':
+        return self.request[DEFAULT_KEY]
