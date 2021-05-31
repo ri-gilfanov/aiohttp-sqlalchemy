8865be1d119b8fdb9c56376187a28e7ede08350b
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-05-31 15:45:54 +0500

    Fix linting views/abc.py

--- aiohttp_sqlalchemy/views/abc.py
+++ aiohttp_sqlalchemy/views/abc.py
@@ -1,6 +1,7 @@
+from abc import ABC
 from aiohttp.abc import AbstractView
 from aiohttp_sqlalchemy.views.mixins import SAViewMixin
 
 
-class AbstractSAView(AbstractView, SAViewMixin):
+class AbstractSAView(ABC, AbstractView, SAViewMixin):
     """ SQLAlchemy abstract class based view mixin. """
