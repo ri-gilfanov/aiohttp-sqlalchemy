2614ff48f421689e0fa6d90512a8f49cc41d2af0
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-06-02 23:34:53 +0500

    Update imports in __init__.py

--- aiohttp_sqlalchemy/__init__.py
+++ aiohttp_sqlalchemy/__init__.py
@@ -1,10 +1,9 @@
 from typing import TYPE_CHECKING
-import warnings
 from aiohttp_sqlalchemy.constants import DEFAULT_KEY
 from aiohttp_sqlalchemy.decorators import sa_decorator
-from aiohttp_sqlalchemy.exceptions import DuplicateAppKeyError
+from aiohttp_sqlalchemy.exceptions import DuplicateAppKeyError, DuplicateRequestKeyError
 from aiohttp_sqlalchemy.middlewares import sa_middleware
-from aiohttp_sqlalchemy.views import SAView, SAViewMixin
+from aiohttp_sqlalchemy.views import SABaseView, SAView, SAViewMixin
 
 
 if TYPE_CHECKING:
@@ -16,6 +15,10 @@ if TYPE_CHECKING:
 
 
 __version__ = '0.4.0'
+
+__all__ = ['DuplicateAppKeyError', 'DuplicateRequestKeyError', 'SABaseView',
+           'sa_bind', 'sa_decorator', 'sa_middleware', 'SAView',
+           'SAViewMixin', 'setup',]
 
 
 def sa_bind(engine: 'AsyncEngine', key: str = DEFAULT_KEY, *,
