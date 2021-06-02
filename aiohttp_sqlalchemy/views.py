d8343410ea7681c24ea02e239149da0fa1ab1212
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-06-02 20:55:21 +0500

    Remove unused imports

--- aiohttp_sqlalchemy/views.py
+++ aiohttp_sqlalchemy/views.py
@@ -1,7 +1,6 @@
 from abc import ABCMeta
 from aiohttp.abc import AbstractView
 from aiohttp.web import View
-from sqlalchemy import delete, insert, join, select, update
 from typing import TYPE_CHECKING
 
 from aiohttp_sqlalchemy.constants import DEFAULT_KEY
@@ -9,7 +8,6 @@ from aiohttp_sqlalchemy.constants import DEFAULT_KEY
 if TYPE_CHECKING:
     from aiohttp.web import Request
     from sqlalchemy.ext.asyncio import AsyncSession
-    from sqlalchemy.sql import Delete, Insert, Join, Select, Update
     from typing import Any
 
 
