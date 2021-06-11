ce8361919047d80ca0075e89113233e9ade81abd
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-06-12 02:31:50 +0500

    Version placeholder 0.11.2

--- aiohttp_sqlalchemy/__init__.py
+++ aiohttp_sqlalchemy/__init__.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
     TSABinding = Tuple[TSessionFactory, str, bool]
 
 
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 
 __all__ = ['DuplicateAppKeyError', 'DuplicateRequestKeyError',
            'SAAbstractView', 'SABaseView', 'sa_bind', 'sa_decorator',
