009c7278b69eeb72efaebbf7a547e19d0666de22
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2021-11-28 20:20:34 +0500

    Migrate from toml to tomli

--- tests/test_aiohttp_sqlalchemy.py
+++ tests/test_aiohttp_sqlalchemy.py
@@ -1,9 +1,10 @@
-import toml
+import tomli
 
 from aiohttp_sqlalchemy import __version__
 
 
 def test_version() -> None:
-    pyproject = toml.load('./pyproject.toml')
-    version = pyproject['tool']['poetry']['version']
-    assert __version__ == version
+    with open('./pyproject.toml', 'rb') as f:
+        pyproject = tomli.load(f)
+        version = pyproject['tool']['poetry']['version']
+        assert __version__ == version
