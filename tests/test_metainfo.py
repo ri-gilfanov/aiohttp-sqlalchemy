891ec6867ab390ae0078f25e5f9fc611293380c0
Ruslan Ilyasovich Gilfanov <ri.gilfanov@yandex.ru>
2024-09-15 11:11:20 +0500

    Dependency optimization: tomli/tomllib

--- tests/test_metainfo.py
+++ tests/test_metainfo.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+import sys
 from pathlib import Path
 
-import tomli
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
 
 from aiohttp_sqlalchemy import __version__
 
 
 def test_version() -> None:
     with Path("./pyproject.toml").open("rb") as f:
-        pyproject = tomli.load(f)
+        pyproject = tomllib.load(f)
         version = pyproject["tool"]["poetry"]["version"]
         assert __version__ == version
